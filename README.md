373c90f1f2d895d6fa65475894d609d6b4a8a3d2
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-03-19 23:01:30 +0300

    done

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+The program evaluates the user's password and gives an estimate from 1 to 10. 1 - very weak password, 10 - very strong password.
+Is taken into account:
+the use of both upper-case and lower-case letters (case sensitivity)
+inclusion of one or more numerical digits
+inclusion of special characters, such as @, #, $
+prohibition of words found in a password blacklist
+prohibition of words found in the user's personal information
+prohibition of passwords that match the format of calendar dates
+
+# Quickstart
+
+The example of script launch on Linux, Python 3.5:
+
+```#!bash
+
+python3 password_strength.py 
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,79 @@
+import re
+import os
 
 
-def get_password_strength(password):
-    pass
+DEFAULT_MIN_PASSWORD_LENGTH = 6
+
+
+def load_data(file_path):
+    with open(file_path, 'rt') as file:
+        return file.read().split('\n')
+
+
+def input_from_file_or_manual(text):
+    work_file = input(text)
+
+    if not os.path.exists(work_file):
+        print('Файл {} не существует'.format(work_file))
+        data = input('Введите данные через пробел:').split()
+        print('='*80)
+    else:
+        data = load_data(work_file)
+
+    return data
+
+
+def is_password_long(password):
+    return (len(password) >= DEFAULT_MIN_PASSWORD_LENGTH)
+
+
+def has_both_upper_and_lower_cases(password):
+    return (not password.isupper() and not password.islower())
+
+
+def has_numerical_digits(password):
+    return bool(re.search('\d+', password))
+
+
+def has_special_char(password):
+    return bool(re.search(r'[ !#$%&():*+,-./[\\\]^_`{|}~]', password))
+
+
+def has_forbidden_words(password, forbidden_words):
+    PATTERN_DD_MM_YYYY = "(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d"
+    PATTERN_MM_DD_YYYY = "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d"
+    has_forbidden_words = bool(re.search(PATTERN_MM_DD_YYYY, password))
+
+    if password in forbidden_words:
+        has_forbidden_words = True
+
+    return has_forbidden_words
+
+
+def get_password_strength(password, forbidden_words):
+    strength = 0
+    if is_password_long(password):
+        strength += 2
+    if has_both_upper_and_lower_cases(password):
+        strength += 3
+    if has_numerical_digits(password):
+        strength += 2
+    if has_special_char(password):
+        strength += 3
+    if has_forbidden_words(password, forbidden_words):
+        strength /= 2
+
+    return strength
 
 
 if __name__ == '__main__':
-    pass
+    
+    password = input('Введите пароль:')
+    
+    blacklist = input_from_file_or_manual('Введите имя файла черного списка:')
+    personal_info = input_from_file_or_manual('Введите имя файла с персональной ифнормацией:')
+    company_name = input_from_file_or_manual('Введите имя файла c полным и кратким названием компании:')
+    forbidden_words = blacklist + personal_info + company_name
+
+    password_strength = get_password_strength(password, forbidden_words)
+    print('Оценка силы пароля: {}'.format(password_strength))
